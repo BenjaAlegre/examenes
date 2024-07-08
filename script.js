cd5db487c7531836dd182475df057f5eea675507
BenjaAlegre <capacitaciones06.02@adviters.com>
2024-07-08 13:52:49 -0300

    promise all

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-const API = 'https://bootcamp-2024-2d43236510d5.herokuapp.com/'
+const API = 'https://bootcamp-2024-2d43236510d5.herokuapp.com'
 
 
 const getPosts = async () => {
     try {
         
 
+        const respAlum = await fetch(`${API}/alumnos`)
+
+        console.log(respAlum);
+
+       // const data = await respAlum.json();
+
+        //console.log(data);  
+
+        const respExa = (await fetch(`${API}/examenes`))
+
+        console.log(respExa);
+
+        const respResp = await fetch(`${API}/respuestas`)
+
+        const promises = [respAlum, respExa, respResp]
+
+        console.log(promises);
+        
+        const promisesJson = promises.map(result => result.json());
+
+        console.log(promisesJson);
+        const datos = await Promise.all(promisesJson);
+
+        const promesas = await Promise.all()
+        console.log(datos);
+
+        //mostrarPosts(pokemons);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+const getPostPoke = async () => {
+    try {
+        //const n = 20;
+        //const pokemonIds = Array.from({ length: n }, (_, i) => i + 1);
+        //const pokemonPromises = pokemonIds.map(id => fetch(API + id).then(resp => resp.json()));
+
         const response = await fetch(`${API}?limit=${limit}&offset=${offset}`)
 
-        console.log(response);
+       // console.log(response);
 
         const data = await response.json();
 
@@ -27,8 +67,6 @@ const getPosts = async () => {
         console.log(error);
     }
 }
-
-
 
 
 function mostrarPosts(posts) {
